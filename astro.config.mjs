4f6a1ed5287acf4602e56018aa27b5d2f43bf872
Tyler Akins <fidian@rumkin.com>
2026-04-24 08:25:35 -0500

    Attempting to use IndexNow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,34 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
-
+import indexnow from 'astro-indexnow';
 import sitemap from '@astrojs/sitemap';
+import { mkdir, writeFile } from 'fs/promises';
+import { promisify } from 'util';
+import { exec } from 'child_process';
 
-// https://astro.build/config
+const integrations = [sitemap()];
+
+if (process.env.INDEXNOW_KEY) {
+    integrations.push(
+        indexnow({
+            key: process.env.INDEXNOW_KEY,
+        })
+    );
+}
+
 export default defineConfig({
     fonts: [
         {
-            provider: fontProviders.google(),
+            provider: fontProviders.fontsource(),
             name: 'Anonymous Pro',
             cssVariable: '--font-anonymous-pro',
         },
         {
-            provider: fontProviders.google(),
+            provider: fontProviders.fontsource(),
             name: 'Ubuntu',
             cssVariable: '--font-ubuntu',
         },
     ],
-    integrations: [sitemap()],
+    integrations,
     site: 'https://metacyclopedia.github.io',
 });
